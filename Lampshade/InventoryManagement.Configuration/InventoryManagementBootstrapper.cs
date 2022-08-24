38b4bf16a83fbb3a6b93f1adb88ac6e8cbe954a6
ALIREZA <mo.alireza_77@yahoo.com>
2022-08-24 16:45:03 -0500

    InventoryApplication Implemented

---
+++ Lampshade/InventoryManagement.Configuration/InventoryManagementBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManagement.Application.Contracts.Inventory;
+using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EFCore.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManagement.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Inventory.Application;
+
+namespace InventoryManagement.Configuration
+{
+    public class InventoryManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IInventoryApplication, InventoryApplication>();
+            services.AddTransient<IInventoryRepository, InventoryRepository>();
+
+            services.AddDbContext<InventoryContext>(x => x.UseSqlServer(connectionString));
+
+        }
+    }
+}
---
+++ Lampshade/InventoyManagmenet.Infrastructure.EFCore/InventoryContext.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EFCore.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Infrastructure.EFCore
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<Inventory> Inventory { get; set; }
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assemble = typeof(InventoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assemble);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
