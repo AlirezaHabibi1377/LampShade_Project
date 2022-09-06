1549a698994b574d17e108c2e2d1a451d4efe5dd
ALIREZA <mo.alireza_77@yahoo.com>
2022-09-06 13:26:11 -0500

    Product Page Implemented

--- Lampshade/01_LampshadeQuery/Query/ProductQuery.cs
+++ Lampshade/01_LampshadeQuery/Query/ProductQuery.cs
@@ -8,6 +8,7 @@ using _01_LampshadeQuery.Contracts.Product;
 using DiscountManagement.Infrastructure.EFCore;
 using InventoryManagement.Infrastructure.EFCore;
 using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductPictureAgg;
 using ShopManagement.Infrastructure.EFCore;
 
 namespace _01_LampshadeQuery.Query
@@ -23,6 +24,95 @@ namespace _01_LampshadeQuery.Query
             _context = context;
             _inventoryContext = inventoryContext;
             _discountContext = discountContext;
+        }
+
+        public ProductQueryModel GetDetails(string slug)
+        {
+            var inventory = _inventoryContext.Inventory
+                .Select(x => new
+                {
+                    x.ProductId,
+                    x.UnitPrice,
+                    x.InStock
+                })
+                .ToList();
+
+            var discounts = _discountContext.CustomerDiscounts
+                .Where(x => x.StartDate < DateTime.Now && x.EndDate > DateTime.Now)
+                .Select(x => new
+                {
+                    x.DiscountRate,
+                    x.ProductId,
+                    x.EndDate
+                })
+                .ToList();
+            
+            var product = _context.Products
+                .Include(x => x.Category)
+                .Include(x=>x.ProductPictures)
+                .Select(product => new ProductQueryModel
+                {
+                    Id = product.Id,
+                    Category = product.Category.Name,
+                    Name = product.Name,
+                    Picture = product.Picture,
+                    PictureAlt = product.PictureAlt,
+                    PictureTitle = product.PictureTitle,
+                    Slug = product.Slug,
+                    CategorySlug = product.Category.Slug,
+                    Code = product.Code,
+                    Description = product.Description,
+                    ShortDescription = product.ShortDescription,
+                    MetaDescription = product.MetaDescription,
+                    keywords = product.Keywords,
+                    Pictures = MapProductPictures(product.ProductPictures)
+                }).FirstOrDefault(x => x.Slug == slug);
+
+            if (product == null)
+            {
+                return new ProductQueryModel();
+            }
+
+            var ProductInventory = inventory.FirstOrDefault(x => x.ProductId == product.Id);
+            if (ProductInventory != null)
+            {
+                product.IsInStock = ProductInventory.InStock;
+                var price = ProductInventory.UnitPrice;
+                
+                product.Price = price.ToMoney();
+                var discount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
+                if (discount != null)
+                {
+                    int discountRate = discount.DiscountRate;
+                    product.DiscountRate = discountRate;
+                    product.DiscountExpireDate = discount.EndDate.ToDiscountFormat();
+                    if (product.DiscountRate > 0)
+                    {
+                        product.HasDiscount = true;
+                    }
+                    else
+                    {
+                        product.HasDiscount = false;
+                    }
+
+                    var discountAmount = Math.Round((price * product.DiscountRate) / 100);
+                    product.PriceWithDiscount = (price - discountAmount).ToMoney();
+                }
+            }
+
+            return product;
+        }
+
+        private static List<ProductPictureQueryModel> MapProductPictures(List<ProductPicture> pictures)
+        {
+            return pictures.Select(x => new ProductPictureQueryModel
+            {
+                IsRemoved = x.IsRemoved,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                ProductId = x.ProductId
+            }).Where(x => !x.IsRemoved).ToList();
         }
 
         public List<ProductQueryModel> GetLatestArrivals()
@@ -55,7 +145,7 @@ namespace _01_LampshadeQuery.Query
                     PictureAlt = product.PictureAlt,
                     PictureTitle = product.PictureTitle,
                     Slug = product.Slug
-                }).OrderByDescending(x=>x.Id).Take(6).ToList();
+                }).OrderByDescending(x => x.Id).Take(6).ToList();
 
             foreach (var product in products)
             {
--- Lampshade/ServiceHost/Pages/ProductCategory.cshtml.cs
+++ Lampshade/ServiceHost/Pages/ProductCategory.cshtml.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using _01_LampshadeQuery.Contracts.ProductCategory;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Pages
