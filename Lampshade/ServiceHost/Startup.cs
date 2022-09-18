6c457705352707a5e99d95fd314e59b544a935c8
ALIREZA <mo.alireza_77@yahoo.com>
2022-09-18 16:40:48 -0500

    Considered SEO of Project

--- Lampshade/ServiceHost/Startup.cs
+++ Lampshade/ServiceHost/Startup.cs
@@ -1,3 +1,5 @@
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using _0_Framework.Application;
 using BlogManagement.Configuration;
 using DiscountManagement.Configuration;
@@ -28,6 +30,8 @@ namespace ServiceHost
             DiscountManagementBootstrapper.Configure(services, connectionString);
             InventoryManagementBootstrapper.Configure(services, connectionString);
             BlogManagementBootstrapper.Configure(services, connectionString);
+
+            services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Arabic));
 
             services.AddTransient<IFileUploader, FileUploader>();
             services.AddRazorPages();
